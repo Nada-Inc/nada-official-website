2509f236943fd43f0ca8cd125b705f70dc8a2afe
ananduremanan <ananduor@gmail.com>
2023-11-30 09:38:58 +0530

    updated website

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,28 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center p-24 bg-black">
+    <main className="flex h-screen items-center justify-center p-24 bg-black overflow-hidden">
       <div>
-        <Image src={"nada.svg"} width={640} height={480} alt="Nada Logo" />
-        <div className="text-gray-500 text-left font-light text-lg md:text-2xl">
-          Beyond Future
+        <div className="flex flex-col items-center w-screen md:flex-row">
+          <div className="md:w-1/2">
+            <Image
+              src={"/hero.png"}
+              width={430}
+              height={430}
+              alt="Nada Logo"
+              className="w-full"
+            />
+          </div>
+          <div className="w-1/2">
+            <div className="text-white text-xl font-bold md:text-8xl">
+              Introducing
+            </div>
+            <Image src={"nada.svg"} width={640} height={480} alt="Nada Logo" />
+            <div className="text-gray-500 text-left font-light text-lg md:text-2xl">
+              Beyond Future
+            </div>
+            <div className="bg-orange-500 h-1 w-20 md:w-60"></div>
+          </div>
         </div>
       </div>
     </main>
